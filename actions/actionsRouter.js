b3b58f426121088c9aaecc3fa5ad34e16889d225
zackhitch <zackhitch@users.noreply.github.com>
2018-04-06 09:39:58 -0700

    WIP - actionsRouter.js - ZH

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -4,4 +4,72 @@ const router = express.Router();
 
 const actionModel = require('../data/helpers/actionModel.js');
 
+router.get('/', (req, res) => {
+  actionModel
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      res.status(500).json(`There was an error GETting actions: ${error}`);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  actionModel
+    .get(id)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(error => {
+      res.status(500).json(`There was an error GETting that action: ${error}`);
+    });
+});
+
+router.post('/', (req, res) => {
+  actionModel
+    .insert(req.body)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(`There was an error POSTing: ${error}`);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  actionModel
+    .update(id, update)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  actionModel
+    .get(id)
+    .then(response => {
+      action = { ...response[0] };
+      actionModel
+        .remove(id)
+        .then(response => {
+          res.status(200).json(action);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    })
+    .catch(error => {
+      res.status(404).json(`Action not found: ${error}`);
+    });
+});
+
 module.exports = router;
