1e622cad19d771de9b8d586ed8f794d03e8fe6af
zackhitch <zackhitch@users.noreply.github.com>
2018-04-06 09:35:03 -0700

    Completed projectsRouter.js - TODO: actionsRouter.js - ZH

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -44,4 +44,48 @@ router.get('/:id/actions', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  projectModel
+    .insert(req.body)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(`There was an error POSTing: ${error}`);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  projectModel
+    .update(id, update)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  projectModel
+    .get(id)
+    .then(response => {
+      project = { ...response[0] };
+      projectModel
+        .remove(id)
+        .then(response => {
+          res.status(200).json(project);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    })
+    .catch(error => {
+      res.status(404).json(`Project not found: ${error}`);
+    });
+});
+
 module.exports = router;
